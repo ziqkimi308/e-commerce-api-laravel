9e9b0e196fae5e7f5c67d7784cc0fed29b0c5fc4
All Fiction <126734865+ziqkimi308@users.noreply.github.com>
2026-05-23 11:40:20 +0800

    make event finished.

---
+++ e-commerce-api-laravel/app/Events/OrderStatusChanged.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Order;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class OrderStatusChanged
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+	public $order;
+	public $oldStatus;
+	public $newStatus;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(Order $order, $oldStatus, $newStatus)
+    {
+        $this->order = $order;
+		$this->oldStatus = $oldStatus;
+		$this->newStatus = $newStatus;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('channel-name'),
+        ];
+    }
+}
